0cd966b25629a1527a231bf34ae06d9bb81c2ee7
Torinad2 <157666959+Torinad2@users.noreply.github.com>
2026-05-27 16:13:43 -0400

    async server

---
+++ functions.js
@@ -0,0 +1,10 @@
+// 1
+const square = function(x) {
+    return x*x;
+};
+
+console.log(square(12))
+
+function square(x) {
+    return x*x
+}
---
+++ test.js
@@ -0,0 +1,36 @@
+// 1
+// let theNumber = Number(prompt("Please enter number of numbers"))
+//
+// if(!Number.isNaN(theNumber)) {
+//     console.log("Please enter number of numbers" + theNumber*theNumber)
+// } else if(theNumber > theNumber) {
+//     console.log("Please enter number of numbers")
+// } else {
+//     console.log("Please enter number of numbers")
+// }
+
+// 2
+// let number = 0
+//
+// while (number <= 12) {
+//     number++
+//     console.log("Please enter number of numbers")
+// }
+
+// 3
+// let yourName;
+// do {
+//     yourName = promt("Please enter your name")
+// } while (!yourName);
+// console.log(yourName + "Is Your name")
+
+// 4
+// let result = 1;
+// for(let counter = 0; counter <10; counter++) {
+//     result += 2
+// }
+// console.log(result);
+
+//5
+let abs = 'abc'
+console.log(abs.length)
