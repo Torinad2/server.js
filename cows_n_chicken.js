732e7c201f8585de88d8bc13e243561e056c193a
Torinad2 <157666959+Torinad2@users.noreply.github.com>
2026-05-30 17:45:16 -0400

    async server

---
+++ cows_n_chicken.js
@@ -0,0 +1,19 @@
+'use strict'
+
+function printFarmInventory(cows, chickens) {
+
+    let cowString = String(cows);
+    while (cowString.length < 3) {
+        cowString = '0' + cowString 
+    }
+    console.log(`${cowString} cows`);
+
+    let chickenString = String(chickens);
+    while (chickenString.length < 3) {
+        chickenString = '0' + chickenString
+    }
+    console.log(chickenString + ' chickens');
+
+}
+
+printFarmInventory(5,44)
